d091afa9d232d511fb032ba83bf3ee9814f708fa
luigi <l.avila.uvg@gmail.com>
2022-12-05 07:41:48 -0600

    CheckBox component added

--- app/src/main/java/com/example/jetpackcomponents/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomponents/MainActivity.kt
@@ -76,10 +76,21 @@ fun DefaultPreview() {
         //MyIcon()
         //MyProgress()
         //MyProgressAdvance()
-        MySwitch()
+        //MySwitch()
+        MyCheckBox()
     }
 }
 /**/
+
+@Composable
+fun MyCheckBox(){
+    var state by rememberSaveable { mutableStateOf(false) }
+    Checkbox(checked = state, onCheckedChange = { state = !state}, enabled = true, colors = CheckboxDefaults.colors(
+        checkedColor = Color.Red,
+        uncheckedColor = Color.Yellow,
+        checkmarkColor = Color.Blue
+    ))
+}
 
 @Composable
 fun MySwitch() {
