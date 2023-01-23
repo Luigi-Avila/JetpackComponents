bcfed48787cde6fc00f100356af026b66d65bd7e
Luigi-Avila <l.avila.uvg@gmail.com>
2023-01-23 12:20:05 -0600

    Row component added

---
+++ app/src/main/java/com/example/jetpackcomponents/components/Row.kt
@@ -0,0 +1,34 @@
+package com.example.jetpackcomponents.components
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showSystemUi = true)
+@Composable
+fun RowPreview(){
+    MyRow()
+}
+
+@Composable
+fun MyRow(){
+    Row(
+        Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())) {
+        Text(text = "Ejemplo 1", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo 1", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo 1", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo 1", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo 1", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo 1", modifier = Modifier.width(100.dp))
+        Text(text = "Ejemplo 1", modifier = Modifier.width(100.dp))
+    }
+}
