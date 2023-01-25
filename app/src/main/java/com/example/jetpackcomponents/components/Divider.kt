624dc31173b1b4cd7214b52e5a8b895b12d238dd
Luigi-Avila <l.avila.uvg@gmail.com>
2023-01-25 11:48:09 -0600

    Divider Component added

--- app/src/main/java/com/example/jetpackcomponents/components/Divider.kt
+++ app/src/main/java/com/example/jetpackcomponents/components/Divider.kt
@@ -1,1 +1,21 @@
 package com.example.jetpackcomponents.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun DividerPreview(){
+    MyDivider()
+}
+
+@Composable
+fun MyDivider() {
+    Divider(Modifier.fillMaxWidth().padding(top = 16.dp), color = Color.Red)
+}
